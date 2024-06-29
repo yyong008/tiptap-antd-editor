512918833a5bca115d6cee78ae6122499751c8c2
Magnesium- <yyong007@outlook.com>
2024-06-29 14:00:08 +0800

    ✨nextjs demo1

---
+++ src/app/demo1/page.tsx
@@ -0,0 +1,5 @@
+import Tiptap from "@/components/tiptap";
+
+export default function Page() {
+  return <Tiptap />
+}
---
+++ todo.md
@@ -0,0 +1,5 @@
+# install tiptap
+
+```sh
+pnpm add @tiptap/react @tiptap/pm @tiptap/starter-kit
+```
