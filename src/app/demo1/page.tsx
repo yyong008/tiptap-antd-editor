a7de355f9e1b5d58fce906cb4812c11576735276
Magnesium- <yyong007@outlook.com>
2024-06-29 20:02:31 +0800

    fix: editor add props

--- src/app/demo1/page.tsx
+++ src/app/demo1/page.tsx
@@ -1,12 +1,12 @@
 import Tiptap from "@/components/tiptap";
-import { Toolbar } from "@/components/toolbar";
+import { useState } from "react";
 
 export default function Page() {
+  const [value, setValue] = useState("");
   return (
     <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-white">
-      
       <div className="w-[50%] max-h-[60vh] border">
-        <Tiptap />
+        <Tiptap value={value} onChange={setValue} />
       </div>
     </div>
   );
