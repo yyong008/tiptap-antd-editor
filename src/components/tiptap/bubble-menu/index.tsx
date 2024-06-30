e9bdc2fc369030fa16436ab32c616a7a6adcebec
Magnesium- <yyong007@outlook.com>
2024-07-01 01:42:41 +0800

    ✨ bubble menu

---
+++ src/components/tiptap/bubble-menu/index.tsx
@@ -0,0 +1,33 @@
+import { BubbleMenu, type Editor } from "@tiptap/react";
+import "./index.css"
+
+export function EditorBubbleMenu({ editor }: { editor: Editor | null }) {
+  return (
+    <>
+      {editor && (
+        <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+          <div className="bubble-menu">
+            <button
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className={editor.isActive("bold") ? "is-active" : ""}
+            >
+              Bold
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              className={editor.isActive("italic") ? "is-active" : ""}
+            >
+              Italic
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              className={editor.isActive("strike") ? "is-active" : ""}
+            >
+              Strike
+            </button>
+          </div>
+        </BubbleMenu>
+      )}
+    </>
+  );
+}
--- src/components/tiptap/index.tsx
+++ src/components/tiptap/index.tsx
@@ -5,7 +5,9 @@ import "./index.css";
 import { EditorContent, useEditor } from "@tiptap/react";
 
 import Blockquote from "@tiptap/extension-blockquote";
+import { Color } from '@tiptap/extension-color'
 import Document from "@tiptap/extension-document";
+import { EditorBubbleMenu } from "./bubble-menu";
 import Heading from "@tiptap/extension-heading";
 import HorizontalRule from "@tiptap/extension-horizontal-rule";
 import Image from "@tiptap/extension-image";
@@ -20,7 +22,7 @@ import TextAlign from "@tiptap/extension-text-align";
 import TextStyle from "@tiptap/extension-text-style";
 import { Toolbar } from "../toolbar";
 import Underline from "@tiptap/extension-underline";
-import { Color } from '@tiptap/extension-color'
+
 const Tiptap = ({ value: content, onChange }: any) => {
   const editor = useEditor({
     extensions: [
@@ -63,6 +65,7 @@ const Tiptap = ({ value: content, onChange }: any) => {
   return (
     <div>
       <Toolbar editor={editor} />
+      <EditorBubbleMenu editor={editor} />
       <EditorContent
         className="h-[200px] p-[10px] overflow-auto"
         editor={editor}
